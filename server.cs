9d89e79421ae580057531050a82b16935103c067
piber20 <piber20@cygo.com>
2017-06-30 15:55:51 -0400

    v9

--- server.cs
+++ server.cs
@@ -247,7 +247,7 @@ function FK_LoadPrefs()
 	if($Pref::Server::FASTKarts::LoadAddon10 $= "")
 		$Pref::Server::FASTKarts::LoadAddon10 = "";
 	
-	$FK::Version = 8.1;
+	$FK::Version = 9;
 }
 
 function FK_RegisterRTBPrefs()
